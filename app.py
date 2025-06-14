883a200c2658c6ac3956332960828d60be5c2994
kidney-bit <karolinewac@gmail.com>
2025-06-14 15:15:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,31 +5,31 @@ from robo_fmabc import executar_robo_fmabc
 from extrator import executar_extrator_tabelado
 from escrivao import enviar_para_google_sheets
 
-st.set_page_config(page_title="Automação FMABC Completa", layout="wide")
-st.title("🧪 Automação Completa: FMABC")
+st.set_page_config(page_title="tablab", layout="wide")
+st.title("🧪 tablab")
 
 # Menu lateral
 aba = st.sidebar.radio("Escolha a funcionalidade:", [
-    "⬇️ Baixar PDFs dos pacientes",
-    "📊 Extrair exames dos PDFs salvos",
-    "📤 Enviar exames para Google Sheets"
+    "⬇️ Baixar PDFs",
+    "📊 Extrair exames dos PDFs",
+    "📤 Enviar exames para o Censo"
 ])
 
-if aba == "⬇️ Baixar PDFs dos pacientes":
+if aba == "⬇️ Baixar PDFs":
     executar_robo_fmabc()
 
-elif aba == "📊 Extrair exames dos PDFs salvos":
+elif aba == "📊 Extrair exames dos PDFs":
     executar_extrator_tabelado()
 
-elif aba == "📤 Enviar exames para Google Sheets":
+elif aba == "📤 Enviar exames para o Censo":
     if "df_exames" in st.session_state:
         url = st.text_input("📎 Cole aqui o link da planilha do Google Sheets:")
         datas_unicas = sorted(st.session_state["df_exames"]["Data"].dropna().unique())
         datas_selecionadas = st.multiselect("📆 Selecione as datas a enviar:", options=datas_unicas)
 
-        if st.button("🚀 Enviar para Google Sheets"):
+        if st.button("🚀 Enviar para o Censo"):
             progresso = st.progress(0)
-            with st.spinner("⏳ Enviando dados para Google Sheets..."):
+            with st.spinner("⏳ Enviando dados para o Censo..."):
                 sucesso = enviar_para_google_sheets(
                     st.session_state["df_exames"],
                     url,
@@ -37,7 +37,7 @@ elif aba == "📤 Enviar exames para Google Sheets":
                     barra_progresso=progresso
                 )
             if sucesso:
-                st.success("✅ Dados enviados com sucesso à planilha!")
+                st.success("✅ Dados enviados com sucesso!")
             else:
                 st.error("❌ Falha ao enviar os dados. Verifique o link e tente novamente.")
     else:
